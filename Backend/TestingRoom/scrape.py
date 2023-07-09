740f894ec04b3cdefb63ba8a9bfd743ef23b9e73
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-07-09 14:14:47 -0400

    Will now link to the actual article

--- Backend/TestingRoom/scrape.py
+++ Backend/TestingRoom/scrape.py
@@ -12,20 +12,23 @@ with open('test.html', "w") as fp:
     fp.write(page.text)
 
 # Parsing time
+link_selector = "#main > div:nth-child({}) > div > a"
 source_selector = "#main > div:nth-child({}) > div > a > div.egMi0.kCrYT > div > div.sCuL3 > div"
 title_selector = "#main > div:nth-child({}) > div > a > div.egMi0.kCrYT > div > div.j039Wc > h3 > div"
 summary_selector = "#main > div:nth-child({}) > div > a > div:nth-child(2) > div:nth-child(2) > div > div > div"
 
-titles, summaries, sources = list(), list(), list()
+links, sources, titles, summaries = [[] for _ in range(4)]
 
 for i in range(4, 14):
-    sources.append(soup.css.select(source_selector.format(i)))
-    titles.append(soup.css.select(title_selector.format(i)))
-    summaries.append(soup.css.select(summary_selector.format(i)))
+    links.append(soup.css.select_one(link_selector.format(i)).get("href").split("?q=")[1].split("&sa")[0])
+    sources.append(soup.css.select_one(source_selector.format(i)).text)
+    titles.append(soup.css.select_one(title_selector.format(i)).text)
+    summaries.append(soup.css.select_one(summary_selector.format(i)).text)
 
 # Aggregation time
 # entries = dict(zip(titles, summaries))
-
+pprint(links[:3], indent=4)
+print("-"*50)
 pprint(sources[:3], indent=4)
 print("-"*50)
 pprint(titles[:3], indent=4)
