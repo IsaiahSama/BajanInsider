5a4382141804ebbef47ef1176ce031c8e62f02df
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-07-09 14:29:38 -0400

    Will now store updates in database

--- Backend/TestingRoom/scrape.py
+++ Backend/TestingRoom/scrape.py
@@ -1,5 +1,7 @@
 from requests import get
 from bs4 import BeautifulSoup
+from sqlite3 import connect
+from os import remove, path
 from pprint import pprint
 
 URL = "https://www.google.com/search?client=opera-gx&hs=boG&sxsrf=AB5stBin05pDbY87etcAQJXd0ANb4Cme2Q:1688919970786&q=Latest+AND+Barbados+AND+news&tbm=nws&sa=X&ved=2ahUKEwjwmKaXhYKAAxVMhIQIHbH1DOgQ0pQJegQIEhAB&biw=1126&bih=599&dpr=1.65"
@@ -12,25 +14,36 @@ with open('test.html', "w") as fp:
     fp.write(page.text)
 
 # Parsing time
+start_index = 4
 link_selector = "#main > div:nth-child({}) > div > a"
 source_selector = "#main > div:nth-child({}) > div > a > div.egMi0.kCrYT > div > div.sCuL3 > div"
 title_selector = "#main > div:nth-child({}) > div > a > div.egMi0.kCrYT > div > div.j039Wc > h3 > div"
 summary_selector = "#main > div:nth-child({}) > div > a > div:nth-child(2) > div:nth-child(2) > div > div > div"
 
-links, sources, titles, summaries = [[] for _ in range(4)]
+# links, sources, titles, summaries = [[] for _ in range(4)]
+entries = {}
 
-for i in range(4, 14):
-    links.append(soup.css.select_one(link_selector.format(i)).get("href").split("?q=")[1].split("&sa")[0])
-    sources.append(soup.css.select_one(source_selector.format(i)).text)
-    titles.append(soup.css.select_one(title_selector.format(i)).text)
-    summaries.append(soup.css.select_one(summary_selector.format(i)).text)
+for i in range(10):
+    index = start_index + i
+    entry = {
+        "LINK": soup.css.select_one(link_selector.format(index)).get("href").split("?q=")[1].split("&sa")[0],
+        "SOURCE": soup.css.select_one(source_selector.format(index)).text,
+        "TITLE": soup.css.select_one(title_selector.format(index)).text,
+        "SUMMARY":soup.css.select_one(summary_selector.format(index)).text
+    }
+    
+    entries[i] = entry 
 
-# Aggregation time
-# entries = dict(zip(titles, summaries))
-pprint(links[:3], indent=4)
-print("-"*50)
-pprint(sources[:3], indent=4)
-print("-"*50)
-pprint(titles[:3], indent=4)
-print("-"*50)
-pprint(summaries[:3], indent=4)
+# Database Setup
+if path.exists("testing.sqlite3"): remove("testing.sqlite3")
+with connect("testing.sqlite3") as db:
+    db.execute("CREATE TABLE TEST (link TEXT NOT NULL, source TEXT NOT NULL, title TEXT UNIQUE NOT NULL, summary TEXT NOT NULL)")
+    db.commit()
+
+# Database addition
+with connect("testing.sqlite3") as db:
+    for e in entries.values():
+        db.execute("INSERT OR REPLACE INTO TEST (link, source, title, summary) VALUES (?, ?, ?, ?)", (e['LINK'], e['SOURCE'], e['TITLE'], e['SUMMARY']))
+        db.commit()
+
+print("Complete")
