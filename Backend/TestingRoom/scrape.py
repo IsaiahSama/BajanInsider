0473bcd05985dfdd11cf098c382d8c07abe931f1
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-07-09 13:51:00 -0400

    Will now select the titles and summaries from the scraped page

--- Backend/TestingRoom/scrape.py
+++ Backend/TestingRoom/scrape.py
@@ -9,3 +9,18 @@ soup = BeautifulSoup(page.text, "html.parser")
 
 with open('test.html', "w") as fp:
     fp.write(page.text)
+
+# Parsing time
+title_selector = "#main > div:nth-child({}) > div > a > div.egMi0.kCrYT > div > div.j039Wc > h3 > div"
+summary_selector = "#main > div:nth-child({}) > div > a > div.egMi0.kCrYT > div > div.j039Wc > h3 > div"
+
+#main > div:nth-child(4) > div > a > div.egMi0.kCrYT > div > div.j039Wc > h3 > div
+#main > div:nth-child(5) > div > a > div.egMi0.kCrYT > div > div.j039Wc > h3 > div
+
+titles, summaries = list(), list()
+for i in range(4, 14):
+    titles.append(soup.css.select(title_selector.format(i)))
+    summaries.append(soup.css.select(summary_selector.format(i)))
+
+print(titles[:5])
+print(summaries[:2])
