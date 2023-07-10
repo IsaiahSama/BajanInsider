4800b9fead1ca42190fb2c3659502cd875dc65ae
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-07-09 20:13:40 -0400

    Added routing for the database

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,6 @@
 # This is where the main Flask Backend will live
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy as sql
 
@@ -18,7 +18,7 @@ class Entry(db.Model):
     title = db.Column(db.String, nullable=False, unique=True)
     summary = db.Column(db.String, nullable=False)
 
-# Creating the data
+# Creating the database
 with app.app_context():
     db.create_all()
 
@@ -27,3 +27,26 @@ with app.app_context():
 @app.route("/")
 def hello_world():
     return "<p>Hello Readers!</p>"
+
+## Database Routing
+@app.route('/add/entries/', methods=["POST"])
+def add_entries():
+    form = request.form
+    
+    entry = Entry(
+        link=form['LINK'],
+        source=form["SOURCE"],
+        title=form["TITLE"],
+        summary=form["SUMMARY"]
+    )
+
+    db.session.add(entry)
+    db.session.commit()
+
+    return "Pretty sure all went well"
+    
+@app.route("/get/entries/all/", methods=["POST"])
+def get_all_entries():
+    entries = db.session.execute(db.Select(Entry))
+
+    return jsonify(entries)
