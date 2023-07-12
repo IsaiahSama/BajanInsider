e0ec163b92a57d337177c0acaf03dae7d067d629
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-07-11 22:54:53 -0400

    Added styling to entries on main page

--- Frontend/bajan-insider/src/App.js
+++ Frontend/bajan-insider/src/App.js
@@ -3,17 +3,30 @@ import Entry from "./Entry";
 
 function App() {
   return (
-    <div className="app">
-      <Entry
-        title="Barbados end CAC Games with nine medals"
-        summary="Barbados ended the Games with two gold medals, two silver medals and five ... For the latest stories and breaking news updates download the...52 mins ago"
-        source="NationNews Barbados"
-        link="https://www.nationnews.com/2023/07/09/barbados-end-cac-games-nine-medals/"
-        date="11/07/2023"
-      />
-      <Entry />
-      <Entry />
-      <Entry />
+    <div className="App">
+      <div className="App_entries">
+        <Entry
+          title="Barbados end CAC Games with nine medals"
+          summary="Barbados ended the Games with two gold medals, two silver medals and five ... For the latest stories and breaking news updates download the...52 mins ago"
+          source="NationNews Barbados"
+          link="https://www.nationnews.com/2023/07/09/barbados-end-cac-games-nine-medals/"
+          date="11/07/2023"
+        />
+        <Entry
+          title="Barbados end CAC Games with nine medals"
+          summary="Barbados ended the Games with two gold medals, two silver medals and five ... For the latest stories and breaking news updates download the...52 mins ago"
+          source="NationNews Barbados"
+          link="https://www.nationnews.com/2023/07/09/barbados-end-cac-games-nine-medals/"
+          date="11/07/2023"
+        />
+        <Entry
+          title="Barbados end CAC Games with nine medals"
+          summary="Barbados ended the Games with two gold medals, two silver medals and five ... For the latest stories and breaking news updates download the...52 mins ago"
+          source="NationNews Barbados"
+          link="https://www.nationnews.com/2023/07/09/barbados-end-cac-games-nine-medals/"
+          date="11/07/2023"
+        />
+      </div>
     </div>
   );
 }
