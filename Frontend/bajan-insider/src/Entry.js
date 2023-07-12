75a95eac2cc1d07f1a20a88e05658dd9e51fa2cd
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-07-11 22:49:15 -0400

    Added styling and props for Entry

--- Frontend/bajan-insider/src/Entry.js
+++ Frontend/bajan-insider/src/Entry.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Entry.css";
 
-const Entry = () => {
+const Entry = ({ title, summary, source, link, date }) => {
   return (
     <div className="Entry">
-      <h1>This represents an Entry</h1>
+      <h2 className="Entry_title">{title}</h2>
+      <p className="Entry_summary">{summary}</p>
+      <div className="Entry_footerContainer">
+        <p>{source}</p>
+        <p>Posted on: {date}</p>
+        <p>
+          {" "}
+          <a href={link} className="Entry_link">
+            Read more...
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
