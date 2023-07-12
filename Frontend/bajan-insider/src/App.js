d35e1dff9ceb295fefb421c55f364d6f62b726e1
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-07-12 00:16:46 -0400

    Removed template

--- Frontend/bajan-insider/src/App.js
+++ Frontend/bajan-insider/src/App.js
@@ -22,13 +22,6 @@ function App() {
       <Header />
       <hr />
       <div className="App_entries">
-        <Entry
-          title="Barbados end CAC Games with nine medals"
-          summary="Barbados ended the Games with two gold medals, two silver medals and five ... For the latest stories and breaking news updates download the...52 mins ago"
-          source="NationNews Barbados"
-          link="https://www.nationnews.com/2023/07/09/barbados-end-cac-games-nine-medals/"
-          date="11/07/2023"
-        />
         {entries.map((entry) => (
           <Entry
             key={entry.id}
