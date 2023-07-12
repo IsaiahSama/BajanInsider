9900b361bc0b7f21f8c06ffbc0db66cf8e0d6c43
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-07-12 00:15:26 -0400

    Will now fetch the Entries from the database

--- Frontend/bajan-insider/src/App.js
+++ Frontend/bajan-insider/src/App.js
@@ -10,8 +10,8 @@ function App() {
 
   useEffect(() => {
     const dataFetch = async () => {
-      // const data = await (await fetch(flaskURL + "/get/15/")).json();
-      const data = await (await fetch("url")).json();
+      const data = await (await fetch(flaskURL + "/get/entries/15")).json();
+      // const data = await (await fetch("url")).json();
       setEntries(data);
     };
     dataFetch();
@@ -29,20 +29,16 @@ function App() {
           link="https://www.nationnews.com/2023/07/09/barbados-end-cac-games-nine-medals/"
           date="11/07/2023"
         />
-        <Entry
-          title="Barbados end CAC Games with nine medals"
-          summary="Barbados ended the Games with two gold medals, two silver medals and five ... For the latest stories and breaking news updates download the...52 mins ago"
-          source="NationNews Barbados"
-          link="https://www.nationnews.com/2023/07/09/barbados-end-cac-games-nine-medals/"
-          date="11/07/2023"
-        />
-        <Entry
-          title="Barbados end CAC Games with nine medals"
-          summary="Barbados ended the Games with two gold medals, two silver medals and five ... For the latest stories and breaking news updates download the...52 mins ago"
-          source="NationNews Barbados"
-          link="https://www.nationnews.com/2023/07/09/barbados-end-cac-games-nine-medals/"
-          date="11/07/2023"
-        />
+        {entries.map((entry) => (
+          <Entry
+            key={entry.id}
+            title={entry.title}
+            summary={entry.summary}
+            source={entry.source}
+            link={entry.link}
+            date={entry.date_scraped}
+          />
+        ))}
       </div>
     </div>
   );
