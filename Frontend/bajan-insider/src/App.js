546a89aa2ebeca8822422e9698a2de62adb38135
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-07-13 07:53:32 -0400

    Removed individual entry rendering from main file.

--- Frontend/bajan-insider/src/App.js
+++ Frontend/bajan-insider/src/App.js
@@ -1,37 +1,14 @@
 import "./App.css";
-import Entry from "./Entry";
+import Entries from "./Entries";
 import Header from "./Header";
 
-import { useEffect, useState } from "react";
-
 function App() {
-  const [entries, setEntries] = useState([]);
-  const flaskURL = "http://127.0.0.1:5000";
-
-  useEffect(() => {
-    const dataFetch = async () => {
-      const data = await (await fetch(flaskURL + "/get/entries/15")).json();
-      // const data = await (await fetch("url")).json();
-      setEntries(data);
-    };
-    dataFetch();
-  }, []);
-
   return (
     <div className="App">
       <Header />
       <hr />
       <div className="App_entries">
-        {entries.map((entry) => (
-          <Entry
-            key={entry.id}
-            title={entry.title}
-            summary={entry.summary}
-            source={entry.source}
-            link={entry.link}
-            date={entry.date_scraped}
-          />
-        ))}
+        <Entries />
       </div>
     </div>
   );
