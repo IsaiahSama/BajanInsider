0b5e2d22fbf51cf439721155cb905881fe1e16bf
IsaiahSama <zeldaplayergl@gmail.com>
2025-04-27 12:35:25 -0400

    Added base for templating

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI, Request
-
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+templates = Jinja2Templates("templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
 async def index(request: Request):
-    return "Hello World!"
+    return templates.TemplateResponse(request, "index.html")
